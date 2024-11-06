78c5040f72d87f215fa4b9c73ce7e1e5d2a2cbe3
adibkn1 <adibkn1@gmail.com>
2024-11-06 23:02:40 +0530

    dw

--- script.js
+++ script.js
@@ -2,16 +2,18 @@
 const onProgress = (event) => {
   const progressBar = event.target.querySelector('.progress-bar');
   const updatingBar = event.target.querySelector('.update-bar');
-  updatingBar.style.width = `${event.detail.totalProgress * 100}%`;
-  if (event.detail.totalProgress === 1) {
-    progressBar.classList.add('hide');
-    event.target.removeEventListener('progress', onProgress);
-  } else {
-    progressBar.classList.remove('hide');
+  if (progressBar && updatingBar) {
+    updatingBar.style.width = `${event.detail.totalProgress * 100}%`;
+    if (event.detail.totalProgress === 1) {
+      progressBar.classList.add('hide');
+      event.target.removeEventListener('progress', onProgress);
+    } else {
+      progressBar.classList.remove('hide');
+    }
   }
 };
 
-document.querySelector('model-viewer').addEventListener('progress', onProgress);
+document.getElementById('ar-viewer').addEventListener('progress', onProgress);
 
 // Launch AR view upon tapping anywhere on the mobile screen
 document.getElementById('mobile-view').addEventListener('click', () => {
